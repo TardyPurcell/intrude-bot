4c7ae94a20f67228577e914da49ec126e2fd93ab
Tardy Purcell <tardypurcell@outlook.com>
2022-08-26 16:11:58 +0800

    feat: add anti_hokp for plugin hokp

--- src/plugins/hokp.rs
+++ src/plugins/hokp.rs
@@ -8,7 +8,8 @@ use crate::models::{CQEvent, Plugin, PluginSenario};
 
 #[derive(Default, Deserialize, Serialize)]
 pub struct HOKpPluginConfig {
-    pub patterns: Vec<String>,
+    pub not_hokp_patterns: Vec<String>,
+    pub hokp_patterns: Vec<String>,
     pub whitelist: Vec<i64>,
 }
 
@@ -23,13 +24,33 @@ impl HOKpPlugin {
         }
     }
     async fn hokp(&self, event: CQEvent, bot: &Bot) -> Result<(), Box<dyn Error + Send>> {
-        let group_id = event.group_id.unwrap();
-        if let None = self.config.whitelist.iter().find(|&&x| x == group_id) {
+        let msg = event.raw_message.unwrap();
+        let mut not_hokp = false;
+        for pattern in self.config.not_hokp_patterns.iter() {
+            let re = Regex::new(pattern).unwrap();
+            if re.is_match(&msg) {
+                not_hokp = true;
+                break;
+            }
+        }
+        if !not_hokp {
             return Ok(());
         }
+        bot.api_request(
+            "send_group_msg",
+            json!({
+                "group_id": event.group_id.unwrap(),
+                "message": "要不咱玩农吧"
+            }),
+        )
+        .await?;
+        Ok(())
+    }
+
+    async fn anti_hokp(&self, event: CQEvent, bot: &Bot) -> Result<(), Box<dyn Error + Send>> {
         let msg = event.raw_message.unwrap();
         let mut is_hokp = false;
-        for pattern in self.config.patterns.iter() {
+        for pattern in self.config.hokp_patterns.iter() {
             let re = Regex::new(pattern).unwrap();
             if re.is_match(&msg) {
                 is_hokp = true;
@@ -43,11 +64,14 @@ impl HOKpPlugin {
             "send_group_msg",
             json!({
                 "group_id": event.group_id.unwrap(),
-                "message": " 要不咱玩农吧"
+                "message": "农批收收味"
             }),
         )
         .await?;
         Ok(())
+    }
+    fn filter(&self, group_id: i64) -> bool {
+        self.config.whitelist.iter().find(|&&x| x == group_id).is_some()
     }
 }
 
@@ -71,7 +95,18 @@ impl Plugin for HOKpPlugin {
 
     async fn handle(&self, event: CQEvent, bot: &Bot) -> Result<(), Box<dyn Error + Send>> {
         match event.post_type.as_str() {
-            "message" => self.hokp(event, bot).await,
+            "message" => match event.message_type.as_ref().unwrap().as_str() {
+                "group" => {
+                    let group_id = event.group_id.clone().unwrap();
+                    if !self.filter(group_id) {
+                        return Ok(());
+                    }
+                    self.hokp(event.clone(), bot).await?;
+                    self.anti_hokp(event, bot).await
+                }
+                "private" => Ok(()),
+                _ => unreachable!(),
+            },
             _ => Ok(()),
         }
     }
