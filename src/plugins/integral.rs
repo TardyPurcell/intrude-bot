e44f064f280d50ffd2374fd16658328b23b8a5e0
Tardy Purcell <tardypurcell@outlook.com>
2022-09-04 03:40:04 +0800

    feat: add integral plugin

--- src/plugins/integral.rs
+++ src/plugins/integral.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 
+use chrono::{Duration, Local, NaiveDateTime};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
 
 use crate::{
@@ -18,6 +20,7 @@ pub struct IntegralPluginConfig {
     db_url: String,
 }
 
+#[allow(dead_code)]
 pub struct IntegralPlugin {
     state: IntegralPluginState,
     config: IntegralPluginConfig,
@@ -39,8 +42,10 @@ impl Plugin for IntegralPlugin {
             ">integral <cmd>\r\n",
             "\r\n",
             "cmd列表:\r\n",
-            "\tpunch   打卡",
-            "\tstatus  查看状态"
+            "\tpunch\t打卡\r\n",
+            "\tstatus\t查看状态\r\n",
+            "\r\n",
+            "24小时内未打卡会导致计时清零"
         )
     }
 
@@ -77,9 +82,81 @@ impl IntegralPlugin {
             return Ok(());
         }
         let cmd = cmd.unwrap();
+        let user_id = event.user_id.unwrap();
+        let group_id = event.group_id.unwrap();
         match cmd {
-            Cmd::Punch => self.punch(event.user_id.unwrap(), bot).await,
-            Cmd::Status => self.status(event.user_id.unwrap(), bot).await,
+            Cmd::Punch => {
+                let res = self.punch(user_id).await;
+                let user_info = bot
+                    .api_request(
+                        "get_group_member_info",
+                        json!(
+                            {
+                                "group_id": group_id,
+                                "user_id": user_id,
+                            }
+                        ),
+                    )
+                    .await?
+                    .json::<DataExtractor>()
+                    .await
+                    .unwrap()
+                    .data;
+                let user_name = match user_info.card {
+                    None => user_info.nickname.unwrap(),
+                    Some(name) => name,
+                };
+                let msg = format!(
+                    "{}已戒导{}秒",
+                    user_name,
+                    res.num_seconds()
+                );
+                bot.api_request(
+                    "send_group_msg",
+                    json!({
+                        "group_id": group_id,
+                        "message": msg
+                    }),
+                )
+                .await?;
+                Ok(())
+            }
+            Cmd::Status => {
+                let res = self.status(user_id).await;
+                let user_info = bot
+                    .api_request(
+                        "get_group_member_info",
+                        json!(
+                            {
+                                "group_id": group_id,
+                                "user_id": user_id,
+                            }
+                        ),
+                    )
+                    .await?
+                    .json::<DataExtractor>()
+                    .await
+                    .unwrap()
+                    .data;
+                let user_name = match user_info.card {
+                    None => user_info.nickname.unwrap(),
+                    Some(name) => name,
+                };
+                let msg = format!(
+                    "{}已戒导{}秒",
+                    user_name,
+                    res.num_seconds()
+                );
+                bot.api_request(
+                    "send_group_msg",
+                    json!({
+                        "group_id": group_id,
+                        "message": msg
+                    }),
+                )
+                .await?;
+                Ok(())
+            }
         }
     }
     fn resolve(msg: String) -> Option<Cmd> {
@@ -91,14 +168,97 @@ impl IntegralPlugin {
             _ => None,
         }
     }
-    async fn punch(&self, user_id: i64, bot: &Bot) -> Result<(), Box<dyn Error + Send>> {
-        Ok(())
+    async fn punch(&self, user_id: i64) -> Duration {
+        let res = self.status(user_id).await;
+        self.update_updated_at_db(user_id).await.ok();
+        res
     }
-    async fn status(&self, user_id: i64, bot: &Bot) -> Result<(), Box<dyn Error + Send>> {
-        Ok(())
+    async fn status(&self, user_id: i64) -> Duration {
+        let now = Local::now().naive_local();
+        match self.get_updated_at_db(user_id).await {
+            Ok(res) => {
+                if now - res >= Duration::days(1) {
+                    self.update_started_at_db(user_id).await.ok();
+                    Duration::zero()
+                } else {
+                    let start_time = self.get_started_at_db(user_id).await.unwrap();
+                    now - start_time
+                }
+            }
+            Err(_) => {
+                self.add_user_db(user_id).await.ok();
+                Duration::zero()
+            }
+        }
     }
-    async fn get_healthy_days_db(&self, user_id: i64) -> Result<i64, Box<dyn Error + Send>> {
-        Ok(0)
+    async fn get_started_at_db(
+        &self,
+        user_id: i64,
+    ) -> Result<NaiveDateTime, Box<dyn Error + Send>> {
+        sqlx::query!(
+            r"SELECT started_at FROM integral_time_card WHERE user_id=$1",
+            user_id
+        )
+        .fetch_one(&self.state.db)
+        .await
+        .map(|row| row.started_at)
+        .map_err(|err| Box::new(err) as Box<dyn Error + Send>)
+    }
+    async fn get_updated_at_db(
+        &self,
+        user_id: i64,
+    ) -> Result<NaiveDateTime, Box<dyn Error + Send>> {
+        sqlx::query!(
+            r"SELECT updated_at FROM integral_time_card WHERE user_id=$1",
+            user_id
+        )
+        .fetch_one(&self.state.db)
+        .await
+        .map(|row| row.updated_at)
+        .map_err(|err| Box::new(err) as Box<dyn Error + Send>)
+    }
+
+    async fn add_user_db(&self, user_id: i64) -> Result<(), Box<dyn Error + Send>> {
+        let now = Local::now().naive_local();
+        let long_time_ago = NaiveDateTime::from_timestamp(0, 0);
+        sqlx::query!(
+            r"INSERT INTO integral_time_card VALUES ($1, $2, $3)",
+            user_id,
+            now,
+            long_time_ago
+        )
+        .execute(&self.state.db)
+        .await
+        .map(|_| ())
+        .map_err(|err| Box::new(err) as Box<dyn Error + Send>)
+    }
+    async fn update_started_at_db(&self, user_id: i64) -> Result<(), Box<dyn Error + Send>> {
+        let now = Local::now().naive_local();
+        sqlx::query!(
+            r"UPDATE integral_time_card
+            SET started_at = $2
+            WHERE user_id = $1",
+            user_id,
+            now
+        )
+        .execute(&self.state.db)
+        .await
+        .map(|_| ())
+        .map_err(|err| Box::new(err) as Box<dyn Error + Send>)
+    }
+    async fn update_updated_at_db(&self, user_id: i64) -> Result<(), Box<dyn Error + Send>> {
+        let now = Local::now().naive_local();
+        sqlx::query!(
+            r"UPDATE integral_time_card
+            SET updated_at = $2
+            WHERE user_id = $1",
+            user_id,
+            now
+        )
+        .execute(&self.state.db)
+        .await
+        .map(|_| ())
+        .map_err(|err| Box::new(err) as Box<dyn Error + Send>)
     }
 }
 
@@ -107,32 +267,13 @@ enum Cmd {
     Status,
 }
 
-#[cfg(test)]
-mod tests {
-    use sqlx::sqlite::SqlitePoolOptions;
+#[derive(Deserialize)]
+struct DataExtractor {
+    data: InnerData,
+}
 
-    #[tokio::test]
-    async fn db_connect() {
-        dotenv::dotenv().ok();
-        let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
-        let pool = SqlitePoolOptions::new().connect(&db_url).await.unwrap();
-        sqlx::query!(
-            r"INSERT INTO integral_time_card VALUES ($1, $2)",
-            0_i64,
-            114514_i64
-        )
-        .execute(&pool)
-        .await
-        .unwrap();
-        let res: i64 = sqlx::query!(
-            r"SELECT healthy_days FROM integral_time_card WHERE user_id=$1",
-            0_i64
-        )
-        .fetch_one(&pool)
-        .await
-        .map(|row| row.healthy_days)
-        .unwrap()
-        .into();
-        assert_eq!(res, 114514);
-    }
+#[derive(Deserialize)]
+struct InnerData {
+    nickname: Option<String>,
+    card: Option<String>,
 }
--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS integral_time_card;
 CREATE TABLE integral_time_card(
     user_id INTEGER PRIMARY KEY,
-    healthy_days INTEGER NOT NULL
+    started_at DATETIME NOT NULL ,
+    updated_at DATETIME NOT NULL
 );
